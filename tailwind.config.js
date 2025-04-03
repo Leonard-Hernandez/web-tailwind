b8febb53bff45eb843d5571ff4d5f48f2400fa26
Leonard Hernandez <leonardhernandez02@gmail.com>
2025-04-02 22:17:00 -0500

    add animation to the logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,28 @@ module.exports = {
             colors: {
                 "azul-claro": "#37bcf9",
                 "azul-oscuro": "#0370b9",
+            },
+            animation: {
+                'spin-low': 'spin 2s linear infinite',
+                'from-bellow': 'fromBellow 500ms linear',
+                'from-right': 'fromRight 500ms linear'
+            },
+            keyframes: {
+                fromBellow: {
+                    '0%': { transform: 'translateY(0%)' },
+                    '50%': { transform: 'translateY(200%)' },
+                    '100%': { transform: 'translateY(0%)' }
+                },
+                fromRight: {
+                    '0%': { transform: 'translataX(0%)' },
+                    '50%': { transform: 'translateX(200%)' },
+                    '100%': { transform: 'translateX(0%)' }
+                }
             }
         }
     },
     variants: {
-
+        animation: ['responsive', 'hover', 'group-hover'],
+        animate: ['responsive', 'hover', 'group-hover']
     }
 }
